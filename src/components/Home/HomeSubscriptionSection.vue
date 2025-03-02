693dcfa858a1317b1cb6961ab9044a1e6c898df7
이희수 <100946922+lee123so@users.noreply.github.com>
2025-03-02 14:10:42 +0900

    Design:스와이퍼 추가

--- src/components/Home/HomeSubscriptionSection.vue
+++ src/components/Home/HomeSubscriptionSection.vue
@@ -10,16 +10,45 @@
         v-model="date"
       ></DatePicker> <!-- ✅ Self-closing 문제 해결 -->
     </div>
-    <div class="w-full mt-6 rounded-lg bg-mono-100">dd</div>
+    <div class="w-full mt-6 px-5 py-5 rounded-lg bg-mono-100">
+    <div>
+      <swiper
+    :direction="'vertical'"
+    :slidesPerView="3"
+    :centeredSlides="true"
+    :pagination="{
+      type: 'progressbar',
+      el:'.progressbar'
+    }"
+    :modules="[Pagination]"
+    class="mySwiper w-full h-[500px]"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+    <div class="swiper-pagination progressbar "></div>
+
+  </swiper>
+    </div>
+  </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import { DatePicker } from 'v-calendar';
-import 'v-calendar/style.css';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Pagination } from 'swiper/modules';
 
 import HomeTitle from './common/HomeTitle.vue';
+
+
+import 'v-calendar/style.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 const date = ref(new Date());
 </script>
@@ -49,4 +78,19 @@ const date = ref(new Date());
 :deep(.vc-day){
   min-height: 45px;
 }
+:deep(.swiper-pagination-progressbar ) {
+    position: absolute;
+    /* top: 300px; */
+    left: 880px;
+    height: 450px;
+    top: 30px;
+    z-index: 1000;
+    width: 4px;
+    background: rgba(0, 0, 0, 0.2);
+  }
+
+  /* Swiper 내부의 프로그레스 바 커스텀 */
+  :deep(.swiper-pagination-progressbar-fill) {
+    background: var(--color-main-400);
+  }
 </style>
