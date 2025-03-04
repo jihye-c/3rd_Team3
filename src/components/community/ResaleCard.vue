e7b7fd1cd1d29c8472813f9bd8d3fb246ae936d1
marie4053 <marie061313@gmail.com>
2025-03-04 12:05:49 +0900

    Feat: 커뮤니티 ResaleCard 컴포넌트 생성

---
+++ src/components/community/ResaleCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  interface Props {
+    image: string;
+    title: string;
+    price: string;
+    dong: string;
+  }
+  defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <div class="w-[380px] h-[380px] overflow-hidden rounded-[12px]">
+      <img :src="image" alt="product" class="w-full h-full object-cover object-center" />
+    </div>
+    <div>
+      <div class="text-[24px] text-mono-700">{{ title }}</div>
+      <div class="text-[28px] text-mono-700 font-bold leading-[30px]">
+        {{ new Intl.NumberFormat().format(Number(price)) }}원
+      </div>
+    </div>
+    <div class="text-[20px] text-mono-400">수유1동</div>
+  </div>
+</template>
+
+<style scoped></style>
