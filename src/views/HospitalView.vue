a1646a4a07204a695ba7ba445000b3915a69d278
jihye choi <jihyec22@gmail.com>
2025-03-05 11:45:31 +0900

    Refactor: 증상 필터 컴포넌트화

--- src/views/HospitalView.vue
+++ src/views/HospitalView.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
   import {ref, onMounted} from 'vue';
-  import {Symptoms} from '@/types/hospitalType.ts';
-  import symptomsData from '@/assets/data/symptoms.json';
-  const symptoms: Symptoms[] = symptomsData as Symptoms[];
+
   import HospitalPostList from '@/components/hospital/HospitalPostList.vue';
   import HospitalDetailCard from '@/components/hospital/HospitalDetailCard.vue';
   import ClinicIcon from '@/assets/icons/clinic.svg';
@@ -12,6 +10,7 @@
   import PublicHealthCenterIcon from '@/assets/icons/publicHealthCenter.svg';
   import OrientalMedicineClinicIcon from '@/assets/icons/orientalMedicineClinic.svg';
   import DentalClinicIcon from '@/assets/icons/dentalClinic.svg';
+import SymptomsFilter from '@/components/hospital/SymptomsFilter.vue';
 
   //-----------------------------화면 제어 관련-----------------------------//
   const selectedHospitalType = ref('clinic'); // 선택된 병원 종류, default 의원
@@ -286,12 +285,6 @@
     window.removeEventListener('mouseup', stopResize);
   };
   
-  //----------------------------- 증상 체크 관련 -----------------------------//
-  const checkedSymptoms = ref([]);
-  const handleChange = () => {
-    console.log(checkedSymptoms.value);
-  };
-
 </script>
 
 <template>
@@ -365,28 +358,7 @@
               v-if="isSymptomButtonShow"
             >
               <div class="text-[20px] font-semibold text-mono-700">증상 선택</div>
-              <form class="flex flex-wrap gap-2 justify-center items-center">
-                <template v-for="(item, index) in symptoms" :key="item.id">
-                  <input
-                    type="checkbox"
-                    class="hidden"
-                    :id="'checkbox-' + item.id"
-                    :value="item.name"
-                    v-model="checkedSymptoms"
-                    @change="handleChange"
-                  />
-                  <label
-                    :for="'checkbox-' + item.id"
-                    class="flex flex-col gap-1.5 justify-center items-center py-3 w-20 h-20 aspect-square bg-main-50 rounded-[12px] border-1 border-main-400 cursor-pointer transition-all"
-                    :class="index === symptoms.length - 1 ? 'col-start-2' : ''"
-                  >
-                    <img :src="item.image" alt="symptom_icon" class="w-8 h-8 select-none" />
-                    <div class="text-[16px] text-main-500 font-medium select-none">
-                      {{ item.name }}
-                    </div>
-                  </label>
-                </template>
-              </form>
+              <SymptomsFilter/>
             </div>
 
             <!-- 리스트 -->
@@ -479,15 +451,5 @@
 
   ::-webkit-scrollbar-track {
     background: transparent; /*스크롤바 뒷 배경 색상*/
-  }
-
-  input[type='checkbox']:checked + label {
-    background-color: var(--color-main-400);
-  }
-  input[type='checkbox']:checked + label div {
-    color: white;
-  }
-  input[type='checkbox']:checked + label img {
-    filter: invert(1);
   }
 </style>
