add8fd5cfef3f512bc5d21aa7c186c2b45fad06c
marie4053 <marie061313@gmail.com>
2025-03-06 16:36:52 +0900

    Feat: 게시글 작성 api 요청 준비

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -5,7 +5,11 @@
     RESALE_CHANNEL_ID,
     REVIEW_CHANNEL_ID,
   } from '@/constants/channelId';
+  import {useAuthStore} from '@/stores/auth';
+  import {useUserStore} from '@/stores/userStore';
+  import {programmersApiInstance} from '@/utils/axiosInstance';
   import {ref, onMounted, nextTick, computed, watch} from 'vue';
+  import {useRouter} from 'vue-router';
 
   interface Channel {
     name: string;
@@ -27,16 +31,26 @@
   };
 
   const selectedChannelId = ref<string | null>(REVIEW_CHANNEL_ID);
+  const selectedTags = ref<string[]>([]);
+  const price = ref<number>();
+  const title = ref<string>('');
+  const content = ref<string>('');
   const selectedImage = ref<string | null>(null);
-  const title = ref('');
-  const content = ref('');
-  const selectedTag = ref('');
-  const price = ref();
 
   const selectedChannel = computed(() => channels.find((c) => c.id === selectedChannelId.value));
   const availableTags = computed(() =>
     selectedChannel.value ? tags[selectedChannel.value.type] || [] : [],
   );
+
+  const router = useRouter();
+
+  // 토큰 가져오기
+  const authStore = useAuthStore();
+  const {token} = authStore;
+
+  // 지역 가져오기
+  const userStore = useUserStore();
+  const {userLocation} = userStore;
 
   const handleImageUpload = (event: Event) => {
     const file = (event.target as HTMLInputElement).files?.[0];
@@ -54,9 +68,35 @@
     }
   };
 
+  const handleSubmit = async () => {
+    console.log(token);
+    //   const response = await programmersApiInstance.post(
+    //     '/post/create',
+    //     {
+    //       title: JSON.stringify({
+    //         title: title.value,
+    //         content: content.value,
+    //         tags: selectedTags.value,
+    //         price: price.value,
+    //         available: true,
+    //         region: userLocation?.address,
+    //       }),
+    //       image: selectedImage.value,
+    //       channelId: selectedChannelId.value,
+    //     },
+    //     {
+    //       headers: {Authorization: `Bearer ${token}`},
+    //     },
+    //   );
+    //   if (response.status === 200 || response.status === 201) {
+    //     alert('작성 성공!');
+    //     router.push(`/community/${selectedChannel.value?.type}`);
+    //   }
+  };
+
   // 채널이 바뀌면 이전 태그값 없애주기
   watch(selectedChannelId, () => {
-    selectedTag.value = '';
+    selectedTags.value = [];
   });
 
   // 페이지가 로드되면 기본 높이 조정
@@ -69,100 +109,108 @@
   <div class="w-full flex flex-col items-center pt-40 pb-12">
     <!-- 페이지 제목 -->
     <h1 class="text-[48px] font-medium text-mono-900">글 작성하기</h1>
-
-    <!-- 채널 선택 -->
-    <div class="mt-8 w-full max-w-[1000px] flex flex-col gap-4">
-      <h2 class="text-[28px] font-medium text-mono-900">게시판 선택하기</h2>
-      <v-select
-        variant="outlined"
-        v-model="selectedChannelId"
-        :items="channels"
-        item-title="name"
-        item-value="id"
-        rounded="lg"
-        class="custom-select w-1/2 text-[20px]"
-      >
-      </v-select>
-    </div>
-
-    <div class="mt-8 w-full max-w-[1000px]">
-      <!-- 가격 선택 -->
-      <div v-if="selectedChannelId === RESALE_CHANNEL_ID" class="flex flex-col gap-4">
-        <h2 class="text-[28px] font-medium text-mono-900">가격</h2>
-        <input
-          v-model="price"
-          type="number"
-          maxlength="20"
-          placeholder="가격을 입력하세요"
-          class="h-[56px] p-4 border-1 border-mono-300 rounded-lg w-full text-[20px]"
-        />
+    <form class="w-full flex flex-col items-center" @submit.prevent="handleSubmit">
+      <!-- 채널 선택 -->
+      <div class="mt-8 w-full flex flex-col gap-4 max-w-[1000px]">
+        <h2 class="text-[28px] font-medium text-mono-900">게시판 선택하기</h2>
+        <v-select
+          variant="outlined"
+          v-model="selectedChannelId"
+          :items="channels"
+          item-title="name"
+          item-value="id"
+          rounded="lg"
+          class="custom-select w-1/2 text-[20px]"
+        >
+        </v-select>
       </div>
-      <!-- 태그 선택 -->
-      <div v-else class="flex flex-col gap-4">
-        <h2 class="text-[28px] font-medium text-mono-900">태그</h2>
-        <div class="flex gap-4 flex-wrap">
-          <label
-            v-for="tag in availableTags"
-            :key="tag"
-            class="px-4 py-1 text-[20px] border-1 rounded-lg cursor-pointer"
-            :class="
-              selectedTag === tag ? 'bg-main-300 text-white' : 'border-mono-300 text-mono-900'
-            "
-          >
-            <input type="radio" v-model="selectedTag" :value="tag" class="hidden" />
-            {{ tag }}
-          </label>
+
+      <div class="mt-8 w-full max-w-[1000px]">
+        <!-- 가격 선택 -->
+        <div v-if="selectedChannelId === RESALE_CHANNEL_ID" class="flex flex-col gap-4">
+          <h2 class="text-[28px] font-medium text-mono-900">가격</h2>
+          <input
+            v-model="price"
+            type="number"
+            maxlength="20"
+            placeholder="가격을 입력하세요"
+            class="h-[56px] p-4 border-1 border-mono-300 rounded-lg w-full text-[20px]"
+            required
+          />
+        </div>
+        <!-- 태그 선택 -->
+        <div v-else class="flex flex-col gap-4">
+          <h2 class="text-[28px] font-medium text-mono-900">태그</h2>
+          <div class="flex gap-4 flex-wrap">
+            <label
+              v-for="tag in availableTags"
+              :key="tag"
+              class="px-4 py-1 text-[20px] border-1 rounded-lg cursor-pointer"
+              :class="
+                selectedTags.includes(tag)
+                  ? 'bg-main-300 text-white'
+                  : 'border-mono-300 text-mono-900'
+              "
+            >
+              <input type="checkbox" v-model="selectedTags" :value="tag" class="hidden" />
+              {{ tag }}
+            </label>
+          </div>
         </div>
       </div>
-    </div>
 
-    <!-- 제목 입력 -->
-    <div class="mt-8 w-full max-w-[1000px] flex flex-col gap-4">
-      <h2 class="text-[28px] font-medium text-mono-900">제목</h2>
-      <input
-        v-model="title"
-        type="text"
-        maxlength="20"
-        placeholder="제목을 작성해주세요 (최대 20자)"
-        class="h-[56px] p-4 border-1 border-mono-300 rounded-lg w-full text-[20px]"
-      />
-    </div>
-
-    <!-- 내용 입력 -->
-    <div class="mt-8 w-full max-w-[1000px] flex flex-col gap-4">
-      <h2 class="text-[28px] font-medium text-mono-900">내용</h2>
-      <textarea
-        v-model="content"
-        ref="contentTextarea"
-        placeholder="내용을 입력해주세요"
-        class="p-4 border-1 border-mono-300 rounded-lg w-full text-[20px] resize-none overflow-hidden"
-        @input="adjustTextareaHeight"
-      ></textarea>
-    </div>
-
-    <!-- 이미지 업로드 -->
-    <div class="mt-8 w-full max-w-[1000px] flex flex-col gap-4">
-      <h2 class="text-[28px] font-medium text-mono-900">이미지 선택하기</h2>
-      <label
-        class="w-full h-[400px] flex items-center justify-center border-1 border-mono-300 rounded-lg cursor-pointer"
-      >
-        <input type="file" class="hidden" @change="handleImageUpload" />
-        <img
-          v-if="selectedImage"
-          :src="selectedImage"
-          width="100px"
-          alt="Uploaded Image"
-          class="w-full h-full object-cover object-center rounded-lg"
+      <!-- 제목 입력 -->
+      <div class="mt-8 w-full max-w-[1000px] flex flex-col gap-4">
+        <h2 class="text-[28px] font-medium text-mono-900">제목</h2>
+        <input
+          v-model="title"
+          type="text"
+          maxlength="20"
+          placeholder="제목을 작성해주세요 (최대 20자)"
+          class="h-[56px] p-4 border-1 border-mono-300 rounded-lg w-full text-[20px]"
+          required
         />
+      </div>
 
-        <v-icon v-else size="80">mdi-image-search-outline</v-icon>
-      </label>
-    </div>
+      <!-- 내용 입력 -->
+      <div class="mt-8 w-full max-w-[1000px] flex flex-col gap-4">
+        <h2 class="text-[28px] font-medium text-mono-900">내용</h2>
+        <textarea
+          v-model="content"
+          ref="contentTextarea"
+          placeholder="내용을 입력해주세요"
+          class="p-4 border-1 border-mono-300 rounded-lg w-full text-[20px] resize-none overflow-hidden"
+          @input="adjustTextareaHeight"
+          required
+        ></textarea>
+      </div>
 
-    <!-- 저장 버튼 -->
-    <button class="mt-10 px-8 py-2 bg-main-400 text-mono-050 text-[20px] font-semibold rounded-lg">
-      저장하기
-    </button>
+      <!-- 이미지 업로드 -->
+      <div class="mt-8 w-full max-w-[1000px] flex flex-col gap-4">
+        <h2 class="text-[28px] font-medium text-mono-900">이미지 선택하기</h2>
+        <label
+          class="w-full h-[400px] flex items-center justify-center border-1 border-mono-300 rounded-lg cursor-pointer"
+        >
+          <input type="file" class="hidden" @change="handleImageUpload" />
+          <img
+            v-if="selectedImage"
+            :src="selectedImage"
+            width="100px"
+            alt="Uploaded Image"
+            class="w-full h-full object-cover object-center rounded-lg"
+          />
+
+          <v-icon v-else size="80">mdi-image-search-outline</v-icon>
+        </label>
+      </div>
+
+      <!-- 저장 버튼 -->
+      <button
+        class="mt-10 px-8 py-2 bg-main-400 text-mono-050 text-[20px] font-semibold rounded-lg"
+      >
+        저장하기
+      </button>
+    </form>
   </div>
 </template>
 
