41db17cc49c76827a14058307c37f4ad1fbbd87a
이희수 <100946922+lee123so@users.noreply.github.com>
2025-03-06 16:35:58 +0900

    Fix: 로그인 라우팅처리 및 새로고침 버그해결

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -14,14 +14,15 @@ export async function userLogin(email: string, password: string): Promise<User> 
   }
   if (res.data?.token) {
     localStorage.setItem('token', res.data.token);
-    return res.data.user;
+    localStorage.setItem('userId', res.data.user._id);
+    return res.data;
   } else {
     throw new Error('로그인 에러 : 토큰이 존재하지 않습니다.');
   }
 }
 
 export async function getUserInfo(id:string):Promise<User>{ //return 어떻게 오는지 봐야함
-  const res = await axiosApi.post(`${apiRoot}/users/${id}`);
+  const res = await axiosApi.get(`${apiRoot}/users/${id}`);
   if(res.status !== 200){
     throw new Error('유저 정보를 가져오던 중 에러가 발생했습니다. : '+ res.status);
   }
